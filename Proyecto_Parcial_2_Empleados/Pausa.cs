c95b7b31a6cb8c6be39a923e6c268a4e07b2b024
Carlos E. Baez <playoung11@gmail.com>
2021-03-26 00:02:52 -0400

    Proyecto parcial 2, ejercicio 2 empleados

---
+++ Proyecto_Parcial_2_Empleados/Pausa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Parcial_2_Empleados
+{
+    class Pausa
+    {
+        public static void PausaFinPrograma()
+        {
+
+            Console.WriteLine("\nPresione cualquier tecla para finalizar el programa.");
+
+            Console.ReadKey();
+
+            Console.Clear();
+
+        }
+
+        public static void PausaLimpiarPantalla()
+        {
+
+            Console.WriteLine("\nPresione cualquier tecla para continuar.");
+
+            Console.ReadKey();
+
+            Console.Clear();
+
+        }
+
+    }
+}
---
+++ Proyecto_Parcial_2_Empleados/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Proyecto_Parcial_2_Empleados
+{
+    class Program
+    {
+        class Empleados
+        {
+            private string[] nombre = new string[5];
+
+            private int[] salario = new int[5];
+
+            private int salarioFijo = 0;
+
+            public void Cargar()
+            {
+                string nombres = "none";
+
+                int contador = -1;
+
+                bool condicionWhile = true;
+
+                bool condicioWhile_2 = true;
+
+
+                for (int i = 0; i <= 4; i++)
+                {
+
+                    ++contador;
+
+                    Console.Write("\nNombre" + "[" + i + "]: ");
+
+                    nombre[i] = Console.ReadLine();
+
+                    condicionWhile = true;
+
+                    while (condicionWhile == true)
+                    {
+                        nombres = nombre[i];
+
+                        int valorNumerico;
+
+                        bool existo = int.TryParse(nombres, out valorNumerico);
+
+                        if (existo)
+                        {
+
+                            Console.WriteLine("\nEsto no es un nombre " + nombres);
+
+                            Console.Write("\nNombre" + "[" + i + "]: ");
+
+                            nombre[i] = Console.ReadLine();
+
+                        }
+                        else
+                        {
+
+                            condicionWhile = false;
+
+                        }
+
+                    }
+
+                    condicioWhile_2 = true;
+
+                    while (condicioWhile_2 == true)
+                    {
+
+                        Console.Write("\nSalario" + "[" + i + "]: ");
+
+                        try
+                        {
+
+                            salario[i] = int.Parse(Console.ReadLine());
+
+                            condicioWhile_2 = false;
+
+                        }
+                        catch
+                        {
+
+                            salario[i] = 0;
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+            public void SalarioMayor()
+            {
+
+                for (int i = 0; i <= 4; i++)
+                {
+
+                    if (salario[salarioFijo] < salario[i])
+                    {
+
+                        salarioFijo = i;
+
+                    }
+
+                    else
+                    {
+
+                    }
+
+                }
+
+                Console.WriteLine("\nEl empleado con el salario mayor es {0}", nombre[salarioFijo], ".");
+
+                Console.WriteLine("\nTiene un salario de {0}", salario[salarioFijo], ".");
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            Empleados em = new Empleados();
+
+            em.Cargar();
+
+            em.SalarioMayor();
+
+        }
+
+    }
+}
