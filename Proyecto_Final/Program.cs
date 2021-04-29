8ea026235a975e5bf752f7f3acc47747f8b8af17
Carlos E. Baez <playoung11@gmail.com>
2021-04-29 04:22:02 -0400

    Proyecto final, sistema calculadora de prestamos, 29/4/2021

---
+++ Proyecto_Final/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Consola_Tabla_de_amortizacion
+{
+    class Program
+    {
+        static bool FinPrograma() 
+        {
+            bool condicionBucle = false;
+            ConsoleKeyInfo Y;
+
+            Console.Write("\nPresione < Enter > si desea continuar el programa...");
+            Y = Console.ReadKey();
+
+            if (Y.Key == ConsoleKey.Enter)
+            {
+                condicionBucle = true;
+                Console.Clear();
+            }
+            else 
+            {
+                Console.Clear();
+                Console.WriteLine("\nFin del programa...");
+                Thread.Sleep(2000);
+            }
+            return condicionBucle;
+        
+        }
+        static void Main(string[] args)
+        {
+           
+            double montoPrestamo = 0, plazoMeses = 0, tasaAnual = 0, cuota, interesMes;
+            double[] evaluadorPrestamos = new double[3] {montoPrestamo, plazoMeses, tasaAnual};
+            bool condicionBucle = true;
+
+            while (condicionBucle == true)
+            {
+
+                Console.WriteLine("\t\t\t\t\t\tCalculador de Prestamos");
+                Console.Write("\nMonto del prestamo (Debe ser en RD$): ");
+                double.TryParse(Console.ReadLine(), out montoPrestamo);
+                Console.Write("\nPLazo (Debe ser en meses y menor a 30 años): ");
+                double.TryParse(Console.ReadLine(), out plazoMeses);
+                Console.Write("\nTasa de Porcentaje Anual (Debe ser en %): ");
+                double.TryParse(Console.ReadLine(), out tasaAnual);
+
+                calculoPrestamo calPres = new calculoPrestamo();
+                tablaAmortizada tabAmor = new tablaAmortizada();
+
+                if ((montoPrestamo > 0) && (plazoMeses > 0) && (plazoMeses <= 360) && (tasaAnual > 0))
+                {
+                    cuota = calPres.CuotaMensual(montoPrestamo, calPres.InteresMes(tasaAnual), plazoMeses);
+                    interesMes = calPres.InteresMes(tasaAnual);
+                    tabAmor.TablaAmortizada(montoPrestamo, tasaAnual, plazoMeses, cuota, interesMes);
+                }
+
+                condicionBucle = FinPrograma();
+            }
+        }    
+    }
+}
---
+++ Proyecto_Final/TablaAmortizada.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class tablaAmortizada
+{
+	private double capital, interes, salddo = 0, totalCapital = 0;
+	private int pago;
+	private DateTime fechaPago;
+
+	public tablaAmortizada()
+	{
+		pago = 0;
+	}
+
+	//Calcula el interes
+	public double Interes(double balance = 0, double interesMes = 0)
+	{
+		double interes;
+		return interes = (balance * interesMes);
+	}
+
+	//Calcula el capital
+	public double Capital(double cuotaMensual = 0, double interes = 0)
+	{
+		double capital;
+		return capital = (cuotaMensual - interes);
+	}
+
+	public void TablaAmortizada(double monto, double tasaAnual, double plazo, double cuota, double interesMes)
+	{
+		Console.Clear();
+		Console.WriteLine("\t\t\t\t\t\tCalculador de Prestamos");
+
+		for (double i = 1; i <= plazo; i++) 
+		{
+			pago++;
+
+			//Este if se encarga del calculo del saldo correspondiente al plazo 1
+			if (i == 1) salddo = monto;
+
+			fechaPago = DateTime.Now.AddDays(29 * pago);
+			this.interes = Math.Round(Interes(salddo, interesMes), 2);
+			this.capital = Math.Round(Capital(cuota, this.interes), 2);
+			totalCapital += this.capital;// Acumulador de capital
+			salddo = Math.Round((monto - totalCapital), 2);
+
+
+			if (i == 1)
+			{
+				Console.WriteLine("\n\tMonto del prestamo en RD$\t\t\t\t\t\t" + monto);
+				Console.WriteLine("\n\tTasa de Porcentaje Anual \t\t\t\t\t\t" + tasaAnual + " %");
+				Console.WriteLine("\n\tPLazo                    \t\t\t\t\t\t" + plazo);
+				Console.WriteLine("\n\tValor Cuota              \t\t\t\t\t\t" + Math.Round(cuota, 2));
+				Console.WriteLine("\n\t\t\t\t\t\tTabla de Amortizacion");
+
+				//Espacio vertical
+				Console.WriteLine();
+				Console.WriteLine();
+			}
+
+			if (i == plazo) salddo = 0;
+			
+            if (i == 1) Console.WriteLine("\tPago\t\tFecha de Pago\t\tCuota\t\tCapital\t\tInteres\t\tBalance");
+            Console.WriteLine("\n\t" + pago + "\t\t" + fechaPago.ToString("d-MMM-yyy") + "\t\t" + Math.Round(cuota, 2) + "\t\t" + this.capital + 
+				"\t\t" + this.interes + "\t\t" + salddo);
+
+		}
+
+	}
+}
